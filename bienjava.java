6c04a9c597c2c7413a4120a56d4e38e9f69e8274
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 09:49:56 +0700

    add

--- bienjava.java
+++ bienjava.java
@@ -1,5 +1,15 @@
 public class bienjava {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        // bien la vi tri, vung nho o trong ram
+        // hoac la tung vi tri nho o trong phan cung
+        // bien co the thay doi duoc
+        
+        // khai bao bien
+        // bien tuy thuoc vao thoi diem gán giá trị
+        // Variable
+        int firstVar = 5;
+
+        System.out.println("firstVar: " + firstVar);
     }
 }
