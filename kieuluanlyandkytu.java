31929660c028501e6de7f01e8212ae7b61a1d386
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 10:49:08 +0700

    add

---
+++ kieuluanlyandkytu.java
@@ -0,0 +1,15 @@
+public class kieuluanlyandkytu {
+    // char 2 bytes
+    // boolean 1 bit
+public static void main(String[] args) {
+    char firstChar = 'I';
+    System.out.println(firstChar);
+
+    // unicode table
+    char secondChar = '\u0040';
+    System.out.println(secondChar);
+
+    boolean isGender;
+}
+
+}
