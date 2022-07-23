6be0b21dba9e8acbb022bb1c345f1a42f0c72179
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 11:36:22 +0700

    add

--- toantu.java
+++ toantu.java
@@ -40,6 +40,42 @@ public class toantu {
         if(newResult>=2) {
             System.out.println("the Result is higher or equal than 1");
         }
+
+        // toan tu && ||
+        // toan tu boolean
+        // toan tu dieu kien
+
+        int firstNumber = 10;
+        if ((firstNumber<5) && (firstNumber<15)) {
+            // ca 2 dieu kien phai dung
+            System.out.println("The number in the range 5 to 15");
+        }
+
+        if ((firstNumber>5) || (firstNumber<10)) {
+            // 1 trong 2 dieu kien dung
+            System.out.println("The number is higher 5 or lower than 10");
+        }
+
+        boolean isBoss= false;
+        if(isBoss ==true) {
+            System.out.println("this is boss");
+        }else{
+            System.out.println("this isn't boss");
+        }
+        
+        if(isBoss=true){
+            System.out.println("why run");
+            // >> day chi la phep gan nen bieu thuc nay van chay
+        }
+
+        // toan tu dieu kien
+        int score = 9;
+        boolean ishighScore= score>8?true:false;
+        if (ishighScore) {
+            System.out.println(" You are get the high score");
+        } else {
+            System.out.println(" You are get lower score");
+        }
     }   
 
     
