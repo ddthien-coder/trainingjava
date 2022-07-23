9aaaec634db0d57415c5a80735df945ebaea22fc
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 10:59:36 +0700

    add

---
+++ KieuString.java
@@ -0,0 +1,21 @@
+public class KieuString {
+    //8 types of primitive data types (kieu du lieu nguyen thuy)
+    
+    //byte 8bit (1byte)
+    //short 16bit (2byte)
+    //int 32bit (4byte)
+    //long 64bit (8byte)
+    //float 64bit (8byte)
+    //double 32bit(4byte)
+    //char 16bit (2byte)
+    //boolean 1bit
+    //--------------//
+    //String = Object
+    // la tap hop 1 chuoi ky tu don
+    public static void main(String[] args) {
+        String firstString ="Day la chuoi ky tu string";
+        System.out.println(firstString);
+    }
+
+
+}
