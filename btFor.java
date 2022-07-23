1e8bc97798f1d2d7c497113327d0176943e893ac
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 14:17:42 +0700

    bt for

---
+++ btFor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class btFor {
+
+    public static void main(String[] args) {
+        System.out.println("Input the number of rows: ");
+        Scanner scanner= new Scanner(System.in);
+        int rows = scanner.nextInt();
+        System.out.println("-----------------------");
+        for (int i=1; i<=rows; i++) {
+            for(int j=1;j<=i;j++) {
+                 // in ra so 1
+                // System.out.print(j);
+                // System.out.print(i);
+                if((i+j)%2==1){
+                    System.out.print(0);
+                } else {
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
