5cf4fa309fd9ece13747f780d0dfd0aeb0eb4154
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 11:56:57 +0700

    add

---
+++ ifelse.java
@@ -0,0 +1,19 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int luongDongThue=5;
+        int luongToi = 10;
+        if (luongToi>luongDongThue) {
+            System.out.println("Toi dong thue");
+        } else {
+            System.out.println("Toi khong dong thue");
+        }
+
+        if (luongToi>20) {
+            System.out.println("Toi phai dong thue rat nhieu");
+        } else if (luongToi>luongDongThue) {
+            System.out.println("Toi dong thue");
+        } {
+            
+        }
+    }
+}
