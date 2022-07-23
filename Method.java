48ae415145762b0a06019876f58b9d30629cf7fe
Tên của bạn <jungjihoonkr97@gmail.com>
2022-07-23 12:45:33 +0700

    add

--- Method.java
+++ Method.java
@@ -10,12 +10,19 @@ public class Method {
 
     public static void main(String[] args) {
         int min;
-        min = findSmallLestNumber(5, 10);
+        min = findSmallLestIn2Number(5, 10);
         System.out.println("So nho nhat trong 2 so 5 vaf 10 la:" + min );
+        int a=5;
+        int b=10;
+        int c=15;
+        int mins = findSmallLestIn3Number(a, b, c);
+        int minss = findSmallLestIn3Number2(a, b, c);
+        System.out.println("So nho nhat trong 3 so " +a +","+b+ "," + c+ " la: "+ mins);
+        System.out.println("So nho nhat trong 3 so " +a +","+b+ "," + c+ " la: "+ minss);
     }
-    
 
-    public static int findSmallLestNumber(int a, int b) {
+
+    public static int findSmallLestIn2Number(int a, int b) {
         int min;
         if(a<=b) {
             min=a;
@@ -23,6 +30,27 @@ public class Method {
             min=b;
         }
         return min;
+    }
+
+
+    public static int findSmallLestIn3Number(int a, int b, int c) {
+        int min=0;
+        min = findSmallLestIn2Number(a, findSmallLestIn2Number(b, c));
+        return min;
+     
+    }
+
+    public static int findSmallLestIn3Number2(int a, int b, int c) {
+        int min;
+        if ((a<=b) && (a<=c)) {
+            min=a;
+        } else if ((b <=a) && (b<=c)) {
+            min=b;
+        } else {
+            min=c;
+        }
+        return min;
+     
     }
 
 }
